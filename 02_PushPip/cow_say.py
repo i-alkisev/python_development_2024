0622932e703765a4a0877730de53d39d987d0485
i-alkisev <i.alkisev@gmail.com>
2024-02-27 00:14:08 +0300

    Hw#2 All options implemented

--- 02_PushPip/cow_say.py
+++ 02_PushPip/cow_say.py
@@ -9,12 +9,70 @@ def parse_args():
         description='Implementation linux command-line utility cowsay via python module'
     )
     parser.add_argument(
+        '-b',
+        action='store_const',
+        const='b',
+        dest='preset'
+    )
+    parser.add_argument(
+        '-g',
+        action='store_const',
+        const='g',
+        dest='preset'
+    )
+    parser.add_argument(
+        '-p',
+        action='store_const',
+        const='p',
+        dest='preset'
+    )
+    parser.add_argument(
+        '-s',
+        action='store_const',
+        const='s',
+        dest='preset'
+    )
+    parser.add_argument(
+        '-t',
+        action='store_const',
+        const='t',
+        dest='preset'
+    )
+    parser.add_argument(
+        '-w',
+        action='store_const',
+        const='w',
+        dest='preset'
+    )
+    parser.add_argument(
+        '-y',
+        action='store_const',
+        const='y',
+        dest='preset'
+    )
+    parser.add_argument(
+        '-pstwy',
+        action='store_const',
+        const='g',
+        dest='preset'
+    )
+    parser.add_argument(
+        '-e',
+    )
+    parser.add_argument(
         '-f'
     )
     parser.add_argument(
         '-l',
         action='store_true',
         help='list available cow pictures'
+    )
+    parser.add_argument(
+        '-T',
+    )
+    parser.add_argument(
+        '-W',
+        type=int
     )
     parser.add_argument(
         'message',
@@ -39,6 +97,14 @@ def main():
         with open(args.f, 'r') as cowfile:
             cow = StringIO(cowfile.read())
         cowsay_params['cowfile'] = cowsay.read_dot_cow(cow)
+    if args.preset is not None:
+        cowsay_params['preset'] = args.preset
+    if args.e is not None:
+        cowsay_params['eyes'] = args.e
+    if args.T is not None:
+        cowsay_params['tongue'] = args.T
+    if args.W is not None:
+        cowsay_params['width'] = args.W
     output = cowsay.cowsay(
         **cowsay_params
     )
